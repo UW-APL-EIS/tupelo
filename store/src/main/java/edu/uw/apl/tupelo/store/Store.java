93f9dc497886c243c88e2079f73d7101720937db
Stuart Maclean <stuart@apl.washington.edu>
2014-10-16 14:22:12 -0700

    Store changes.  About to be moved to store-api module

--- store/src/main/java/edu/uw/apl/tupelo/store/Store.java
+++ store/src/main/java/edu/uw/apl/tupelo/store/Store.java
@@ -1,8 +1,11 @@
 package edu.uw.apl.tupelo.store;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.UUID;
 
+import edu.uw.apl.tupelo.model.ManagedDisk;
+import edu.uw.apl.tupelo.model.ManagedDiskDescriptor;
 import edu.uw.apl.tupelo.model.Session;
 
 public interface Store {
@@ -12,6 +15,15 @@ public interface Store {
 	public long getUsableSpace() throws IOException;
 	
 	public Session newSession() throws IOException;
+
+	public void put( ManagedDisk md ) throws IOException;
+
+	public Collection<ManagedDiskDescriptor> enumerate() throws IOException;
+
+	/**
+	 * Use with caution, could LOSE data
+	 */
+	//	public void clear();
 }
 
 // eof
