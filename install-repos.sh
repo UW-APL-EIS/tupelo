9522b62879dfaed3dad36491a10b339554edee91
Stuart Maclean <stuart@apl.washington.edu>
2014-10-24 09:57:37 -0700

    Maven in quiet mode for repo install script

--- install-repos.sh
+++ install-repos.sh
@@ -3,10 +3,10 @@
 # Copy into our local Maven repo those Tupelo dependencies 
 # for which we may not have sources.  We need a Maven repository!
 
-mvn install:install-file -Dfile=repos/native-lib-loader-1.0.1.jar
+mvn -q install:install-file -Dfile=repos/native-lib-loader-1.0.1.jar
 
-mvn install:install-file -Dfile=repos/fuse4j-core-2400.0.0.jar
+mvn -q install:install-file -Dfile=repos/fuse4j-core-2400.0.0.jar
 
-mvn install:install-file -Dfile=repos/tsk4j-core-413.0.0.jar
+mvn -q install:install-file -Dfile=repos/tsk4j-core-413.0.0.jar
 
 # eof
