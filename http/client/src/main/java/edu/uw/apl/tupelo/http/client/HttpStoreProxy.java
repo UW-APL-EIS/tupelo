f282eea676c08fcd2fc2c1eb163138955a0f83ed
Stuart Maclean <stuart@apl.washington.edu>
2014-11-20 11:07:27 -0800

    supply impl for expanded store api

--- http/client/src/main/java/edu/uw/apl/tupelo/http/client/HttpStoreProxy.java
+++ http/client/src/main/java/edu/uw/apl/tupelo/http/client/HttpStoreProxy.java
@@ -130,9 +130,25 @@ public class HttpStoreProxy implements Store {
 	}
 
 	@Override
-	public UUID uuid( ManagedDiskDescriptor mdd ) {
-		// LOOK TODO
-		return null;
+	public UUID uuid( ManagedDiskDescriptor mdd ) throws IOException {
+		HttpGet g = new HttpGet( server + "disks/data/uuid/" + mdd.getDiskID() +
+								   "/" + mdd.getSession() );
+		log.debug( g.getRequestLine() );
+		g.addHeader( "Accept", "application/x-java-serialized-object" );
+		log.debug( g.getRequestLine() );
+		HttpClient req = new DefaultHttpClient( );
+		HttpResponse res = req.execute( g );
+		HttpEntity he = res.getEntity();
+		InputStream is = he.getContent();
+		ObjectInputStream ois = new ObjectInputStream( is );
+		try {
+			UUID result = (UUID)ois.readObject();
+			return result;
+		} catch( ClassNotFoundException cnfe ) {
+			throw new IOException( cnfe );
+		} finally {
+			ois.close();
+		}
 	}
 	
 	/**
