7bf36297d35b5eddd2ca4dc0e5a9dbc0125c5319
Stuart Maclean <stuart@apl.washington.edu>
2015-01-23 12:42:11 -0800

    add package-level javadoc file

---
+++ http/server/src/main/java/edu/uw/apl/tupelo/http/server/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides the classes necessary to communicate with a Tupelo store
+ * via http/https.
+ *
+ * <p>
+ * The package involves several servlet classes, supporting
+ * classes dealing with * global state management via the {@link
+ * javax.servlet.ServletContext} and some .jsp files for presentation,
+ */
+package edu.uw.apl.tupelo.http.server;
